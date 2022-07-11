7b04f528eb2d1bc463fa2fec1857dd534194c39e
vmoreirav <vmoreira.m09@gmail.com>
2022-07-11 20:10:30 -0300

    first commit

---
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Book } from "./model/Book";
+
+@Injectable() 
+
+export class BookService
+{
+    private url - 
+}
